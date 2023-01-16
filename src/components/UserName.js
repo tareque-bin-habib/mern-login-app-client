726605329a15f6ea6fddbad53e23849543d48d3f
tareque-bin-habib <shad.tbh@gmail.com>
2023-01-16 14:45:11 +0600

    user login complete

--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import avatar from '../assets/profile.png'
+import styles from '../styles/UserName.module.css'
 const UserName = () => {
     return (
-        <div>
-            <h1>user</h1>
+        <div className='container mx-auto'>
+            <div className='flex justify-center items-center h-screen'>
+                <div className={styles.glass}>
+                    <div className='title flex flex-col items-center'>
+                        <h4 className='text-5xl font-bold'>Hello Again!</h4>
+                        <span className='py-4 text-xl w-2/3 text-center text-gray-500'>Explore more by connecting with us.</span>
+                    </div>
+                    <form className='py-1'>
+                        <div className='profile flex justify-center py-4'>
+                            <img src={avatar} className={styles.profile_img} alt="avatar" />
+                        </div>
+                        <div className='textbox flex flex-col items-center gap-6'>
+                            <input className={styles.textbox} type="text" placeholder='Enter your username' />
+                            <button className={styles.btn} type='submit'>Let's GO</button>
+                        </div>
+                        <div className='text-center py-4'>
+                            <span className='text-gray-500'>Not a member <Link to='/register' className='text-red-500'>Please register</Link> </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
