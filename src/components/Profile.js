5fb97c50918207f184cbf97b7234dbd3c41d639c
tareque-bin-habib <shad.tbh@gmail.com>
2023-01-27 15:50:42 +0600

    profile ui done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import avatar from '../assets/profile.png'
+import styles from '../styles/UserName.module.css'
+import { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { registerValidation } from '../helper/Validate'
+import convertToBase64 from '../helper/Convert'
+
 
 const Profile = () => {
+
+    const [file, setFile] = useState()
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            userName: '',
+            password: ''
+        },
+        validate: registerValidation,
+        validateOnBlur: false,
+        validateOnChange: false,
+
+        onSubmit: async values => {
+            values = await Object.assign(values, { profile: file || '' })
+            console.log(values)
+        }
+
+    });
+
+    const onUpload = async e => {
+        const base64 = await convertToBase64(e.target.files[0]);
+        setFile(base64)
+    }
+
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className='container mx-auto'>
+            <Toaster position='top-center' reverseOrder={false}></Toaster>
+            <div className='flex justify-center items-center h-screen'>
+                <div className={styles.glass} style={{ width: '45%', height: '85%' }}>
+                    <div className='title flex flex-col items-center'>
+                        <h4 className='text-5xl font-bold'>Profile</h4>
+                        <span className='py-4 text-xl w-2/3 text-center text-gray-500'>You can update the details</span>
+                    </div>
+                    <form onSubmit={formik.handleSubmit} className='py-1'>
+                        <div className='profile flex justify-center py-4'>
+                            <label htmlFor="profile">
+                                <img src={file || avatar} className={styles.profile_img} alt="avatar" />
+                            </label>
+                            <input onChange={onUpload} type="file" id="profile" name='profile' />
+                        </div>
+                        <div className='textbox flex flex-col items-center gap-6'>
+                            <div className='name flex w-3/4 gap-10'>
+                                <input {...formik.getFieldProps('firstname')} className={styles.textbox} type="text" placeholder='First name' />
+                                <input {...formik.getFieldProps('lastname')} className={styles.textbox} type="text" placeholder='Last name' />
+                            </div>
+                            <div className='name flex w-3/4 gap-10'>
+                                <input {...formik.getFieldProps('mobile')} className={styles.textbox} type="text" placeholder='Mobile Number' />
+                                <input {...formik.getFieldProps('email')} className={styles.textbox} type="email" placeholder='Enter your email' />
+                            </div>
+
+                            <input {...formik.getFieldProps('address')} className={styles.textbox} type="text" placeholder='Address' />
+                            <button className={styles.btn} type='submit'>Update</button>
+
+
+
+                        </div>
+                        <div className='text-center py-4'>
+                            <span className='text-gray-500'>Come back later? <Link to='/' className='text-red-500'>Log out</Link> </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
